1dba4b5211fdae511e319868b49af88f113d6561
hexer-13 <zorikan8@gmail.com>
2026-06-27 16:27:21 +0300

    lesson 16(OOP3)

--- hw-current/Ex1.py
+++ hw-current/Ex1.py
@@ -1,60 +1,39 @@
-import datetime
+# Прізвище
+# Група
+# Фізика
+# Інформ
+# Історія
+#
+# Визначити середній бал оцінок з фізики, кількість студентів з оцінкою 5 з інформатики та вивести відомості про них.
 
-class Product:
-    def __init__(self, seller, name, amount, price, date):
-        self.__seller = seller
-        self.__name = name
-        self.__amount = amount
-        self.__price = price
-        self.__date = date
-
-    @property
-    def seller(self):
-        return self.__seller
-
-    @property
-    def name(self):
-        return self.__name
+class Grades:
+    def __init__(self, surname,group,physics,informatics,history):
+        self.__surname = surname
+        self.__group = group
+        self.__physics = physics
+        self.__informatics = informatics
+        self.__history = history
 
     @property
-    def amount(self):
-        return self.__amount
+    def surname(self):
+        return self.__surname
 
     @property
-    def price(self):
-        return self.__price
+    def group(self):
+        return self.__group
 
     @property
-    def date(self):
-        return self.__date
+    def physics(self):
+        return self.__physics
+
+    @property
+    def informatics(self):
+        return self.__informatics
+
+    @property
+    def history(self):
+        return self.__history
 
 
-    def __str__(self):
-        return f"{self.name}, {self.amount}, {self.price}, {self.date}"
 
-
-Product1 = Product("Іванов", "Knife", 10,100, datetime.date(2026, 6, 25))
-Product2 = Product("Seller2", "Boiler", 2,200, datetime.date(2026, 1, 25))
-Product3 = Product("Іванов", "Pan", 3,300, datetime.date(2001, 6, 25))
-Product4 = Product("Seller4", "Rope", 4,400, datetime.date(2026, 6, 7))
-Product5 = Product("Seller5", "Bag", 5,500, datetime.date(2026, 1, 1))
-
-products = [Product1, Product2, Product3, Product4, Product5]
-
-def count_product_amount_by_sellers(products_in,seller_in):
-    count = 0
-    res_list = []
-    max_cost = 0
-    for product in products_in:
-        if product.seller == seller_in:
-            count += product.amount
-            res_list.append(str(product))
-            if product.price > max_cost:
-                max_cost = product.price
-        elif product.seller != seller_in and product == products_in[-1] and res_list == []:
-            return "Нема продавця з таким іменем"
-    print(f"Seller: {seller_in}, quantity of products: {count}, max cost: {max_cost}, products info: {res_list}")
-    return count, res_list, max_cost
-
-res = count_product_amount_by_sellers(products,"Іванов")
-print(res)
+Student1 = Grades("Surname1",)
